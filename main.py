c43035409810d180f1d902f933fb1293cdb60400
Gabriel Eger <gabriel_eger01@hotmail.com>
2023-01-03 18:03:29 -0300

    image to text conversion with pytesseract
    converting the reshaped image to text pytesseract.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import cv2 as cv
 import numpy as np
+import pytesseract as tess
+tess.pytesseract.tesseract_cmd = r'C:\PYTESSERCT LOCATION\tesseract.exe'
 
 #This function is going to be useful to set the right canny in the GUI
-# def drawRec(biggestNew, mainFrame):
-#         cv.line(mainFrame, (biggestNew[0][0][0], biggestNew[0][0][1]), (biggestNew[1][0][0], biggestNew[1][0][1]), (0, 255, 0), 20)
-#         cv.line(mainFrame, (biggestNew[0][0][0], biggestNew[0][0][1]), (biggestNew[2][0][0], biggestNew[2][0][1]), (0, 255, 0), 20)
-#         cv.line(mainFrame, (biggestNew[3][0][0], biggestNew[3][0][1]), (biggestNew[2][0][0], biggestNew[2][0][1]), (0, 255, 0), 20)
-#         cv.line(mainFrame, (biggestNew[3][0][0], biggestNew[3][0][1]), (biggestNew[1][0][0], biggestNew[1][0][1]), (0, 255, 0), 20)
+
+def drawRec(biggestNew, mainFrame):
+        cv.line(mainFrame, (biggestNew[0][0][0], biggestNew[0][0][1]), (biggestNew[1][0][0], biggestNew[1][0][1]), (0, 255, 0), 20)
+        cv.line(mainFrame, (biggestNew[0][0][0], biggestNew[0][0][1]), (biggestNew[2][0][0], biggestNew[2][0][1]), (0, 255, 0), 20)
+        cv.line(mainFrame, (biggestNew[3][0][0], biggestNew[3][0][1]), (biggestNew[2][0][0], biggestNew[2][0][1]), (0, 255, 0), 20)
+        cv.line(mainFrame, (biggestNew[3][0][0], biggestNew[3][0][1]), (biggestNew[1][0][0], biggestNew[1][0][1]), (0, 255, 0), 20)
 
 #importing and resizing the image, need to set a universal resize method for different papers sheet in the future
 
-image = cv.imread("IMAGE LOCATION")
+image = cv.imread("IMAGE_NAME.jpg")
 w, h = 500, 700
 image = cv.resize(image, (int(w * 2), int(h * 2)))
-imgWarp = image.copy()
+image_warp = image.copy()
 
 # Apllying effects to find the canny and corners of papers
 
@@ -38,19 +41,28 @@ for c in contours: #for to find all the edges of the opaper
 
 #drawing the edges of the paper with the drawRec func and resizing to a plain paper:
 
-# if len(biggest_contour) != 0:
-#     biggest_contour = biggest_contour.reshape((4, 2))
-#     biggestNew = np.zeros((4, 1, 2), dtype= np.int32)
-#     add = biggest_contour.sum(1)
-#     biggestNew[0] = biggest_contour[np.argmin(add)]
-#     biggestNew[3] = biggest_contour[np.argmax(add)]
-#     dif = np.diff(biggest_contour, axis = 1)
-#     biggestNew[1] = biggest_contour[np.argmin(dif)]
-#     biggestNew[2] = biggest_contour[np.argmax(dif)]
-#     drawRec(biggestNew, corner_frame)
-#     corner_frame = cv.drawContours(corner_frame, biggestNew, -1, (255, 0, 255), 25)
-#     pts1 = np.float32(biggestNew)
-#     pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
-#     matrix = cv.getPerspectiveTransform(pts1, pts2)
-#     imgWarp = cv.warpPerspective(image, matrix, (w, h))
+if len(biggest_contour) != 0:
+    biggest_contour = biggest_contour.reshape((4, 2))
+    new_biggest = np.zeros((4, 1, 2), dtype= np.int32)
+    add = biggest_contour.sum(1)
+    new_biggest[0] = biggest_contour[np.argmin(add)]
+    new_biggest[3] = biggest_contour[np.argmax(add)]
+    difference = np.diff(biggest_contour, axis = 1)
+    new_biggest[1] = biggest_contour[np.argmin(difference)]
+    new_biggest[2] = biggest_contour[np.argmax(difference)]
+    drawRec(new_biggest, corner_frame)
+    corner_frame = cv.drawContours(corner_frame, new_biggest, -1, (255, 0, 255), 25)
+    pts1 = np.float32(new_biggest)
+    pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+    matrix = cv.getPerspectiveTransform(pts1, pts2)
+    image_warp = cv.warpPerspective(image, matrix, (w, h))
 
+#showing final image to user:
+
+cv.imshow('image', image_warp)
+cv.waitKey(0)
+
+#converting image to text with pytesseract:
+
+text = tess.image_to_string(image_warp)
+print(text)
