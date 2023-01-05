0a7672cf020f7d1887da9c47ab06164749abb931
Gabriel Eger <gabriel_eger01@hotmail.com>
2023-01-05 19:55:27 -0300

    Regular expression and program changes
    regular expressions added, now you can filter the generated text to find the important data, in this case I used a medical prescription form, but that can be a adapted, also I've decided to remove the GUI part from the project so I can focus in other things, I may add it in the future.

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import cv2 as cv
 import numpy as np
 import pytesseract as tess
-tess.pytesseract.tesseract_cmd = r'C:\PYTESSERCT LOCATION\tesseract.exe'
+import re
 
-#This function is going to be useful to set the right canny in the GUI
+tess.pytesseract.tesseract_cmd = r'C:\TESSERACT LOCATION\tesseract.exe'
 
-def drawRec(biggestNew, mainFrame):
-        cv.line(mainFrame, (biggestNew[0][0][0], biggestNew[0][0][1]), (biggestNew[1][0][0], biggestNew[1][0][1]), (0, 255, 0), 20)
-        cv.line(mainFrame, (biggestNew[0][0][0], biggestNew[0][0][1]), (biggestNew[2][0][0], biggestNew[2][0][1]), (0, 255, 0), 20)
-        cv.line(mainFrame, (biggestNew[3][0][0], biggestNew[3][0][1]), (biggestNew[2][0][0], biggestNew[2][0][1]), (0, 255, 0), 20)
-        cv.line(mainFrame, (biggestNew[3][0][0], biggestNew[3][0][1]), (biggestNew[1][0][0], biggestNew[1][0][1]), (0, 255, 0), 20)
 
 #importing and resizing the image, need to set a universal resize method for different papers sheet in the future
 
-image = cv.imread("IMAGE_NAME.jpg")
-w, h = 500, 700
-image = cv.resize(image, (int(w * 2), int(h * 2)))
+image = cv.imread("IMAGE NAME.jpg")
+w, h = image.shape[1], image.shape[0]
+image = cv.resize(image, (827, 1170))
 image_warp = image.copy()
 
 # Apllying effects to find the canny and corners of papers
 
 gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY) #turn image to gray
 blurred_image = cv.GaussianBlur(gray_image, (5, 5), 1) #add blur
-canny_image = cv.Canny(blurred_image, 190, 190) #trying to find the canny, this current value (190, 190) is not universal and will be set by the user with the drawRec func
+canny_image = cv.Canny(blurred_image, 190, 190) #trying to find the canny, this current value (190, 190) is not universal, but should work in the majority of the cases
 contours, _ = cv.findContours(canny_image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 countor_image = image.copy()
 countor_image = cv.drawContours(countor_image, contours, -1, (255, 0, 255), 4)
@@ -50,7 +45,6 @@ if len(biggest_contour) != 0:
     difference = np.diff(biggest_contour, axis = 1)
     new_biggest[1] = biggest_contour[np.argmin(difference)]
     new_biggest[2] = biggest_contour[np.argmax(difference)]
-    drawRec(new_biggest, corner_frame)
     corner_frame = cv.drawContours(corner_frame, new_biggest, -1, (255, 0, 255), 25)
     pts1 = np.float32(new_biggest)
     pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
@@ -58,11 +52,28 @@ if len(biggest_contour) != 0:
     image_warp = cv.warpPerspective(image, matrix, (w, h))
 
 #showing final image to user:
-
-cv.imshow('image', image_warp)
+image_show = cv.resize(image_warp, (827, 1170))
+cv.imshow('image', image_show)
 cv.waitKey(0)
 
 #converting image to text with pytesseract:
 
 text = tess.image_to_string(image_warp)
+united_text = text.replace("\n", "")
 print(text)
+medicine_text = text.replace("\n", "&")
+
+#Filtering text with regular expression:
+
+name = str(re.search(r'Name: (.*?)Age', united_text).group(1))
+age = str(re.search(r'Age: (.*?)Address', united_text).group(1))
+address = str(re.search(r'Address: (.*?)Date', united_text).group(1))
+date = str(re.search(r'Date: (.*?)Rx', united_text).group(1))
+medicine = str(re.search(r'Rx&&(.*?)Refill', medicine_text).group(1)).replace('&', ' ') #text manipulation with & in order to get the spaces in medicine
+refill = str(re.search(r'(?<=Refill: )(.*)', united_text).group(1))
+
+
+important_data = [name, age, address, date, medicine, refill]
+
+for data in important_data:
+    print(data)
